f636464a070a496b5462b1baa6c4c628b25668ce
Prog. Apps Android L y M de 15:30 a 20:30 <android-ed1@aula.local>
2016-04-27 18:14:43 +0200

    27/04/2016 Ciclo de vida

--- app/src/main/java/info/wwwood/lifecicle/MainActivity.java
+++ app/src/main/java/info/wwwood/lifecicle/MainActivity.java
@@ -6,7 +6,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -18,11 +25,50 @@ public class MainActivity extends Activity {
     private int countOnStop=0;
     private int countOnDestroy=0;
 
+    private final int tvOnCreateID=0;
+    private final int tvOnRestartID=1;
+    private final int tvOnStartID=2;
+    private final int tvOnResumeID=3;
+    private final int tvOnPauseID=4;
+    private final int tvOnStopID=5;
+    private final int tvOnDestroyID=6;
+
+    private List<TextView> tvList=new ArrayList<TextView>();
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("OnCreate ",String.valueOf(++countOnCreate));
+
+        TextView tvOnCreate=new TextView(this);
+        tvOnCreate.setTag(tvOnCreateID);
+        tvList.add(tvOnCreate);
+
+        TextView tvOnRestart=new TextView(this);
+        tvOnRestart.setTag(tvOnRestartID);
+        tvList.add(tvOnRestart);
+
+        TextView tvOnStart=new TextView(this);
+        tvOnStart.setTag(tvOnStartID);
+        tvList.add(tvOnStart);
+
+        TextView tvOnResume=new TextView(this);
+        tvOnResume.setTag(tvOnResumeID);
+        tvList.add(tvOnResume);
+
+        TextView tvOnPause=new TextView(this);
+        tvOnPause.setTag(tvOnPauseID);
+        tvList.add(tvOnPause);
+
+        TextView tvOnStop=new TextView(this);
+        tvOnStop.setTag(tvOnStopID);
+        tvList.add(tvOnStop);
+
+        TextView tvOnDestroy=new TextView(this);
+        tvOnDestroy.setTag(tvOnDestroyID);
+        tvList.add(tvOnDestroy);
+
     }
 
     @Override
@@ -41,6 +87,7 @@ public class MainActivity extends Activity {
     protected void onResume() {
         super.onResume();
         Log.d("onResume ",String.valueOf(++countOnResume));
+        renderView();
     }
 
     @Override
@@ -63,6 +110,47 @@ public class MainActivity extends Activity {
     }
 
     private void renderView(){
-        
+        ViewGroup lyMain=(ViewGroup) findViewById(R.id.lyMain);
+        lyMain.removeAllViews();
+
+        int MarginTop=0;
+
+        for (TextView textView:tvList){
+            RelativeLayout.LayoutParams params=new RelativeLayout.LayoutParams(
+                    RelativeLayout.LayoutParams.WRAP_CONTENT,
+                    RelativeLayout.LayoutParams.WRAP_CONTENT
+            );
+            params.addRule(RelativeLayout.ALIGN_PARENT_LEFT,RelativeLayout.TRUE);
+            params.addRule(RelativeLayout.ALIGN_PARENT_TOP,RelativeLayout.TRUE);
+
+            params.topMargin=MarginTop;
+
+            switch ((Integer) textView.getTag())
+            {
+                case tvOnCreateID:
+                    textView.setText("onCreate " + String.valueOf(countOnCreate));
+                    break;
+                case tvOnRestartID:
+                    textView.setText("onRestart " + String.valueOf(countOnRestart));
+                    break;
+                case tvOnStartID:
+                    textView.setText("onStart " + String.valueOf(countOnStart));
+                    break;
+                case tvOnResumeID:
+                    textView.setText("onResume " + String.valueOf(countOnResume));
+                    break;
+                case tvOnPauseID:
+                    textView.setText("onPause " + String.valueOf(countOnPause));
+                    break;
+                case tvOnStopID:
+                    textView.setText("onStop " + String.valueOf(countOnStop));
+                    break;
+                case tvOnDestroyID:
+                    textView.setText("onDestroy " + String.valueOf(countOnDestroy));
+                    break;
+            }
+            lyMain.addView(textView,params);
+            MarginTop=MarginTop+=100;
+        }
     }
 }
